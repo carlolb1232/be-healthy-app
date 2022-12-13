d1350a07b2d33ac17d053e02db2bd3960598dabe
Danilorca <daniela.lorca.r@gmail.com>
2022-12-13 13:25:17 -0300

    avance dashboard usuario

--- client/src/views/MainUserDashboard.js
+++ client/src/views/MainUserDashboard.js
@@ -1,14 +1,43 @@
 import React from 'react';
 import img_dashboard from "../assets/objetivo-usuario.png"
-
+import styles from "./styles_modules/MainUserDashboard.module.css"
 const MainUserDashboard = () => {
 
   return (
-    <div>
+    <div className={styles.container}>
       <img src={img_dashboard} alt="imagen de dashboard" />
-      <h1>
-        MAIN CLIENTE
-      </h1>
+      <div>
+        <h3>Registros mensuales</h3>
+        <div>
+          <table className="table table-dark">
+            <thead>
+              <tr>
+                <th>Fecha</th>
+                <th>Peso (Kg)</th>
+                <th>Altura (cm)</th>
+                <th>IMC</th>
+                <th>% grasa</th>
+                <th>Calorías</th>
+                <th>Rutinas</th>
+                <th>Dietas</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>02/01/2022</td>
+                <td>74</td>
+                <td>170</td>
+                <td>22.5</td>
+                <td>Sobrepeso</td>
+                <td>30</td>
+                <td>1800</td>
+                <td><button>ver</button></td>
+                <td><button>ver</button></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
